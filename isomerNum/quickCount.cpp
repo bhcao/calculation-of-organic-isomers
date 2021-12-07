892433cd7d29255568999935e617d9aad1fa8ae1
Bovera <1847845057@qq.com>
2021-12-07 23:15:34 +0800

    stable

--- isomerNum/quickCount.cpp
+++ isomerNum/quickCount.cpp
@@ -1,12 +1,16 @@
+// "in_" means do not use it outside
+
+namespace qct{
+
 int in_alkyl(int n, int* pAlkyl){
     int sum = 0;
     n--;
-    if(n%3==0) //三个均相等
+    if(n%3==0) // three are the same
         sum += pAlkyl[n/3]*(pAlkyl[n/3]+1)*(pAlkyl[n/3]+2)/6;
     for(int a=n/3+1;a<=n;a++){
-        if(a<=n/2) // 最大的相等
+        if(a<=n/2) // biggest are the same
             sum += pAlkyl[a]*(pAlkyl[a]+1)*pAlkyl[n-2*a]/2;
-        if((n-a)%2==0) // 最小的相等
+        if((n-a)%2==0) // smallest are the same
             sum += pAlkyl[a]*pAlkyl[(n-a)/2]*(pAlkyl[(n-a)/2]+1)/2;
         int b=(n-a)/2+1;
         while(b<=n-a && b<a){
@@ -24,29 +28,29 @@ for(int i=0; i<=n; i++){ \
         pAlkyl[i] = 1; \
     else \
         pAlkyl[i] = in_alkyl(i, pAlkyl); \
-} // 必须与 delete []pAlkyl 配合使用
+} // DO NOT forget to use "delete []pAlkyl"
 
 int alkyl(int n){
     ALKYL_LIST
-    int final = pAlkyl[n];
+    int finals = pAlkyl[n];
     delete []pAlkyl;
-    return final;
+    return finals;
 }
 
 int in_signAlkane(int n, int* pAlkyl){
     int sum = 0; --n;
-    if(n%4==0) //四个均相等
+    if(n%4==0) // four are the same
         sum += pAlkyl[n/4]*(pAlkyl[n/4]+1)*(pAlkyl[n/4]+2)*(pAlkyl[n/4]+3)/24;
-    for(int i=0;i<=n/3;i++) //三个相等
+    for(int i=0;i<=n/3;i++) // three are the same
         if(n%4!=0||i!=n/4)
             sum += pAlkyl[i]*(pAlkyl[i]+1)*(pAlkyl[i]+2)*pAlkyl[n-3*i]/6;
     if(n%2==0){
-        for(int i=0;i<n/4;i++) //两组两个相等
+        for(int i=0;i<n/4;i++) // two group with two are the same
             sum += pAlkyl[i]*(pAlkyl[i]+1)*(pAlkyl[n/2-i]+1)*pAlkyl[n/2-i]/4;
         if(n%4!=0)
             sum += pAlkyl[n/4]*(pAlkyl[n/4]+1)*pAlkyl[n/2-n/4]*(pAlkyl[n/2-n/4]+1)/4;
     }
-    for(int i=0;i<=n/2;i++) //一组两个相等
+    for(int i=0;i<=n/2;i++) // one group with two are the same
         for(int j=0;j<=n;j++)
             if(n-2*i-j<j && n-2*i-j!=i && i!=j && n-2*i-j>=0)
                 sum += pAlkyl[i]*(pAlkyl[i]+1)*pAlkyl[n-2*i-j]*pAlkyl[j]/2;
@@ -71,18 +75,20 @@ int in_monoAlkene(int n, int *pAlkyl){
 
 int monoAlkene(int n){
     ALKYL_LIST
-    int final = in_monoAlkene(n, pAlkyl);
+    int finals = in_monoAlkene(n, pAlkyl);
     delete []pAlkyl;
-    return final;
+    return finals;
 }
 
 int alkane(int n){
     ALKYL_LIST
-    int final;
+    int finals;
     if(n%2==0)
-        final = in_signAlkane(n, pAlkyl)-in_monoAlkene(n, pAlkyl)+pAlkyl[n/2];
+        finals = in_signAlkane(n, pAlkyl)-in_monoAlkene(n, pAlkyl)+pAlkyl[n/2];
     else
-        final = in_signAlkane(n, pAlkyl)-in_monoAlkene(n, pAlkyl);
+        finals = in_signAlkane(n, pAlkyl)-in_monoAlkene(n, pAlkyl);
     delete []pAlkyl;
-    return final;
+    return finals;
 }
+
+} // namespace end
